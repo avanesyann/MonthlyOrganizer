a09e84e9b85d072d2bdef701171a174accdb6e0d
Narek Avanesyan <avanesyan12345@gmail.com>
2025-03-28 23:26:41 +0400

    Initialize project files.

---
+++ Organize.cs
@@ -0,0 +1,40 @@
+﻿namespace MonthlyOrganizer
+{
+    public class Organize
+    {
+        private DirectoryInfo UserDirectory { get; }
+
+        public Organize(string path)
+        {
+            if (Directory.Exists(path))
+                UserDirectory = new DirectoryInfo(path);
+            else
+                throw new DirectoryNotFoundException("The directory does not exist.");
+        }
+
+        public void OrganizeByMonth()
+        {
+            var files = UserDirectory.GetFiles();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Last write time: {file.LastWriteTime.Month} {file.LastWriteTime.Year}");
+            }
+        }
+
+
+        public string CreateDirectory(string folderName)
+        {
+            string newDirectory = Path.Combine(UserDirectory.FullName, folderName);
+
+            if (!Directory.Exists(newDirectory))
+            {
+                Directory.CreateDirectory(newDirectory);
+
+                Console.WriteLine($"Directory \"{newDirectory}\" created.");
+            }
+
+            return newDirectory;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MonthlyOrganizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change this to your folder's path
+            string path = @"C:\Users\Me\Desktop\recordings";
+
+            Organize organize = new Organize(path);
+
+            organize.OrganizeByMonth();
+        }
+    }
+}
